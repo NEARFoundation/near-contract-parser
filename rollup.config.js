d5ec725e3f0598bbc8c88ee52b6536545fa82ca9
Jacob <encody@users.noreply.github.com>
2022-02-02 10:54:05 -0600

    removes commented-out code

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,11 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-	input: 'lib/index.js',
-	output: {
-		file: 'dist/bundle.js',
-		format: 'umd',
-		name: 'nearContractParser',
-		// globals: {'@webassemblyjs/wasm-parser': 'wasmParser'},
-	},
-	// external: ['@webassemblyjs/wasm-parser'], // <-- suppresses the warning
-	plugins: [commonjs(), nodeResolve()]
+  input: 'lib/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'umd',
+    name: 'nearContractParser',
+  },
+  plugins: [commonjs(), nodeResolve()],
 };
